880370454bcc3bd25ea028b530037cb8cbfd5405
Diego Delfin <diegodelfinacharte@gmail.com>
2016-04-02 16:54:23 -0500

    estructura  persona , asignar datos y mostrarlos

--- main.go
+++ main.go
@@ -4,6 +4,21 @@ import (
 	"fmt"
 )
 
+type Persona struct {
+	nombres   string
+	apellidos string
+	edad      int
+}
+
 func main() {
-	fmt.Println("Hola Mundo !")
+
+	var per Persona // declaramos una variable de tipo Persona struct
+
+	per.nombres = "Bryan Diego"    // Asignamos el campo nombres
+	per.apellidos = "Perez Delfin" // Asignamos el campo apellidos
+	per.edad = 23                  // Asignamos el campo edad
+
+	// Mostramos por consola los campos de la estructura
+	fmt.Println("La persona ha sido creada y tiene nombres - apellidos - edad", per.nombres, per.apellidos, per.edad)
+
 }
